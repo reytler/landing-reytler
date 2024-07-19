7e89d12d7f073d2421c47c38a1acf18a689611ba
Reytler Souza <reytlercompare@hotmail.com>
2024-07-19 15:44:54 -0300

    Conteúdo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { links, rating, textAprender, textPorque, textSobre, textSobreAulas } from "./texts";
+import { link } from "fs";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -11,21 +14,74 @@ export default function Home() {
         <div className={styles.center}>
           <section about="Informações sobre o professor de programação" className="about">
             <article>
-              <p>Sobre</p>
+              <h2>Sobre o professor</h2>
+              <p>
+                {textSobre}
+              </p>
+            </article>
+            <article>
+              <h2>O que você vai aprender?</h2>
+              <p>
+                {textAprender}
+              </p>
+            </article>
+            <article>
+              <h2>Sobre as aulas</h2>
+              <p>
+                {textSobreAulas}
+              </p>
+            </article>
+            <article>
+              <h2>Por que dou aula?</h2>
+              <p>
+                {textPorque}
+              </p>
             </article>
           </section>
           <section about="Avaliações e comentários sobre o professor de programação" className="rating">
-            <article>
-              <p>Avaliações</p>
-            </article>
+            <h2>Avaliações e recomendações</h2>
+            {
+              rating.map((item,_idx)=>(
+                <>
+                  <article>
+                    <h3>{item.name}</h3>
+                    <p>{item.text}</p>
+                  </article>
+                </>
+              ))
+            }
+          </section>
+          <section about="Obtenha acesso às aulas de programação" className={styles.btnctaarea}>
+            <Link href={links.superprof}>
+              <button className={styles.btncta}>Quero ter acesso às aulas</button>
+            </Link>
           </section>
         </div>
-        <aside about="Redes sociais do professor de programação" className="social">
-          <p>Redes Sociais</p>
+        <aside about="Redes sociais do professor de programação" className={styles.social}>
+          <span>
+            <h4>Linkedin</h4>
+            <a href={links.linkedin}>Icone</a>
+          </span>
+          <span>
+            <h4>Github</h4>
+            <a href={links.github}>Icone</a>
+          </span>
+          <span>
+            <h4>Instagram</h4>
+            <a href={links.instagram}>Icone</a>
+          </span>
+          <span>
+            <h4>Superprof</h4>
+            <a href={links.superprof}>Icone</a>
+          </span>
         </aside>
       </section>
       <footer className={styles.footer}>
-        rodapé
+        <p>
+          Copyright © Reytler Souza
+          <br />
+          Rmss &nbsp; - 2024
+        </p>
       </footer>
     </main>
   );
---
+++ src/app/texts.ts
@@ -0,0 +1,52 @@
+export const textSobre = `
+    Sou graduado em Sistemas de Informação pelo Centro Universitário de Mineiros. Atualmente faço parte do setor de tecnologia em saúde em uma grande empresa do segmento.
+    Tive o privilégio de participar de projetos desafiadores como implantação de Sistemas, entre eles, sistemas de Gestão Hospitalar, ERP, Sistemas de Finanças, RH, Contábeis e ETC.
+
+    Sendo um profissional organizado, comunicativo e com flexibilidade e de rápida aprendizagem, características essas que me auxiliaram ao longo de minha jornada.
+    Possuo a capacidade analítica, resolução de problemas, planejamento estratégico.
+`
+
+export const textAprender = `
+    Programação da teoria à prática de forma descomplicada.
+`
+export const textSobreAulas = `
+    Aprender programação do conceito e aplicar na vida real é a melhor forma de aprendizado. 
+    Aulas descomplicadas, com bastante teoria e o mais divertido, a prática! O objetivo é simplificar o complexo para caber na mente e a partir daí conseguir realizar coisas incríveis!
+`
+
+export const textPorque = `
+    Por amor à área de tecnologia, tenho o desejo de incentivar iniciantes à se tornarem ótimos profissionais através da didática e prática, 
+    descomplicando a forma de aprendizagem e mostrando que à tecnologia é para todos e não um bicho de 7 cabeças.
+`
+
+export const rating = [
+    {
+        name: `Iarla Raifa`,
+        text: `O professor Reytler tem uma ótima didática e paciência, possui amplo conhecimento em programação e na área da tecnologia no geral.`
+    },
+    {
+        name: `Willians Dias`,
+        text: `Ele sempre está disposto a ajudar os outros e é muito paciente e atencioso. Ele também é muito organizado e eficiente, o que o torna um ótimo professor. Ele sempre consegue explicar os conceitos de forma clara e concisa, e ele é sempre paciente com os alunos que estão tendo dificuldades. Eu recomendo fortemente o Reytler como professor. Ele é um ótimo professor e uma ótima pessoa.
+
+            Nos conheceços na epóca da faculdade, onde fizemos todos o curso de graduação juntos, sempre adimirei o reytler como aluno e como pessoa. Sempre me aopio e me ajudou nos estudos na graduação e vida profissional pós faculdade.`
+    },
+    {
+        name: `Renato Francischini`,
+        text: `
+            Tenho o prazer de recomendar Reytler, um professor de programação excepcional. Com vasta experiência em ensino e desenvolvimento de software, ele se destaca por sua excelência técnica, criatividade e comprometimento com a qualidade. Reytler é um educador talentoso que sabe transmitir conceitos complexos de maneira clara e acessível. Sua abordagem didática e prática inspira e motiva seus alunos a atingirem todo o seu potencial. Se você procura um professor de programação dedicado e inovador, Reytler é a escolha certa.
+        `
+    },
+    {
+        name: `Matheus Maffra`,
+        text: `
+            É um profissional dedicado e altamente qualificado, com uma vasta experiência em programação e uma paixão inigualável pelo conhecimento. Com certeza o Reylter proporcionará uma educação de qualidade e um ambiente de aprendizado proveitoso para todos os alunos.
+        `
+    }
+]
+
+export const links = {
+    linkedin: `https://www.linkedin.com/in/reytlersouza-programador/`,
+    github: `https://github.com/reytler`,
+    instagram: `https://www.instagram.com/reytlermatheus/`,
+    superprof: `https://www.superprof.com.br/programacao-teoria-pratica-forma-descomplicada-sou-graduado-sistemas-informacao-pelo-centro-universitario.html`
+}
